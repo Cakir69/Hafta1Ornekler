01cfeb5b5a02169bcb1306fb5848b36ac7606cbc
Cakir69 <cakira2005@gmail.com>
2025-10-15 16:36:18 +0300

    Create Sinif.py

---
+++ Sinif.py
@@ -0,0 +1,24 @@
+"""
+Spyder Editor
+aracılığıyla sınıf yapısına giriş-1
+"""
+# %%
+a=5
+string="dogus"
+#ctrl+enter run
+#%% Class-sınıflar
+isci_ismi="ali"
+isci_yas= 50
+isci_adres="Üsküdar"
+isci_kurum="dogus"
+"""her çalışan için
+tanımlama yapmayızz
+sınıf oluştururuz
+"""
+#class ortak nesnelere ait özellikleri bir arada tutan yapıdır.
+class Calisan():
+#özellikler; yaş,adres,tc,ad,soyad........vs
+#metot/davranışlar; çalışma,terfi alma.....vss
+pass #içeriği sonra oluşturacağım
+
+calisan1=Calisan() #nesne türetildi.
